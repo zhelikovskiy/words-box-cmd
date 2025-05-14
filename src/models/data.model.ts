56e017fbd6d18ef3e618374856801471efc981e1
Artem Zhelikovskyi <zhelikovskiy.artem@gmail.com>
2025-05-14 17:50:51 +0200

    Update db data model

--- src/models/data.model.ts
+++ src/models/data.model.ts
@@ -2,8 +2,14 @@ import Dictionary from './dictionary.model';
 import Word from './word.model';
 
 interface Data {
-	dictionaries: Dictionary[];
-	words: Word[];
+	dictionaries: {
+		lastId: number;
+		data: Dictionary[];
+	};
+	words: {
+		lastId: number;
+		data: Word[];
+	};
 }
 
 export default Data;
